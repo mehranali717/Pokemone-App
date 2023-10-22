78253040ee032fcc1818f5490c38f0be28de1acf
mehranali717 <Mehranalee110@gmail.com>
2023-10-23 02:22:40 +0500

    view Detail is added

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -3,48 +3,143 @@ import { useGetPokemonByNameQuery } from "../../Redux/Apis/ApiSlice";
 import { useFavorites } from "../../Hooks/useFavorite";
 import Button from "../Button/Button";
 import Image from "../Image/Image";
+import { useNavigate } from "react-router";
 
-const PokemonCard =({pokemone})=> {
-    const [favorites, setAddRemovFavorites] = useFavorites("favorites");
-    const {data:PokemonData, isLoading:pokemoneLoading} = useGetPokemonByNameQuery(pokemone);
-    if(pokemoneLoading) {
-        return "isLoading";
-    }
-    const isFavorite = favorites.some((favorite) => favorite.name === PokemonData.name);
-    return  <div className="border  p-[20px] rounded-lg bg-[#f0f0f0] shadow-xl   w-[400px]">
-                <div className="px-2 py-4">
-                <div className="flex justify-between items-center text-[22px] text-[#333] font-sans font-bold capitalize gap-x-[20px]">
-                    <h2 className="block "> {PokemonData.name}</h2>
-                    <FontAwesomeIcon
-                    onClick={()=>setAddRemovFavorites(PokemonData)}
-                    icon="heart"
-                    className={`float-right cursor-pointer w-[27px] h-[27px] ${isFavorite ? 'text-[red]' : 'text-gray-500'}`}/>
-                </div>
-                <div
-                    className="my-[30px] mx-auto w-[150px] h-[150px] 
+const PokemonCard = ({ pokemone, page="" }) => {
+  const [favorites, setAddRemovFavorites] = useFavorites("favorites");
+  const navigate = useNavigate();
+  const { data: PokemonData, isLoading: pokemoneLoading } =
+    useGetPokemonByNameQuery(pokemone);
+  console.log({ PokemonData });
+  if (pokemoneLoading) {
+    return "isLoading";
+  }
+  const isFavorite = favorites.some(
+    (favorite) => favorite.name === PokemonData.name
+  );
+  const imageSrcs = [
+    PokemonData.sprites.back_default,
+    PokemonData.sprites.back_shiny,
+    PokemonData.sprites.front_default,
+    PokemonData.sprites.front_shiny,
+  ];
+  const viewDetailHandler =(name)=> {
+        navigate(`/detail/${name}`,{
+            state: {
+                name: name,
+              },
+        });
+  }
+  return (
+    <div className="border  p-[20px] rounded-lg bg-[#f0f0f0] shadow-xl   w-[400px]">
+      <div className="px-2 py-4">
+        <div className="flex justify-between items-center text-[22px] text-[#333] font-sans font-bold capitalize gap-x-[20px]">
+          <h2 className="block "> {PokemonData.name}</h2>
+          <FontAwesomeIcon
+            onClick={() => setAddRemovFavorites(PokemonData)}
+            icon="heart"
+            className={`float-right cursor-pointer w-[27px] h-[27px] ${
+              isFavorite ? "text-[red]" : "text-gray-500"
+            }`}
+          />
+        </div>
+        <div
+          className="my-[30px] mx-auto w-[150px] h-[150px] 
                         p-[5px] bg-white shadow-xl
                         rounded-full"
-                >
-                   <Image image={PokemonData.sprites.front_default} />
-                </div>
-                <div className="py-[20px] border-b flex border-b-[#4f4f4f] justify-between">
-                    <h3 className="block text-center text-[18px] text-[#333] font-sans font-bold">
-                    Weight
-                    </h3>
-                    <p className="block text-center text-[18px] text-[#333] font-sans font-bold">
-                    {PokemonData.weight}lbs
-                    </p>
-                </div>
-                <div className="py-[20px] flex justify-between">
-                    <h3 className="block text-center text-[18px] text-[#333] font-sans font-bold">
-                    Height
-                    </h3>
-                    <p className="block text-center text-[18px] text-[#333] font-sans font-bold">
-                    {PokemonData.height}cm
-                    </p>
-                </div>
-                </div>
-                <Button value="View Details" />
-  </div>
-}
+        >
+          <Image image={PokemonData.sprites.front_default} />
+        </div>
+        <div className="py-[20px] border-b flex justify-between">
+          <h3 className="block text-center text-[18px] text-[#333] font-sans font-bold capitalize">
+            Height
+          </h3>
+          <span className="block text-center text-[18px] text-[#333] font-sans font-bold">
+            {toCentimeters(PokemonData.height)}cm
+          </span>
+        </div>
+        <div className="py-[20px] flex justify-between">
+          <h3 className="block text-center text-[18px] text-[#333] font-sans font-bold capitalize">
+            Weight
+          </h3>
+          <span className="block text-center text-[18px] text-[#333] font-sans font-bold">
+            {toKilograms(PokemonData.weight)} kg
+          </span>
+        </div>
+        {(page === "detail" )&& <>
+        <div className="py-[20px] border-b">
+          <h3 className="block pb-[15px] text-center text-[18px] text-[#333] font-sans font-bold">
+            Types
+          </h3>
+          <ul className="flex justify-center gap-x-[10px] ">
+            {PokemonData.types.map((type, index) => (
+              <li
+                key={index}
+                className="px-[18px] py-[8px] font-bold text-[#111827] border-2 border-[#111827] rounded-full capitalize"
+              >
+                {type.type.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="py-[20px] border-b">
+          <h3 className="block text-center pb-[15px] text-[18px] text-[#333] font-sans font-bold">
+            Abilities
+          </h3>
+          <ul className="flex justify-center gap-x-[10px]">
+            {PokemonData.abilities.map((ability, index) => (
+              <li
+                key={index}
+                className="px-[18px] py-[8px] font-bold text-[#111827] border-2 border-[#111827] rounded-full capitalize"
+              >
+                {ability.ability.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="py-[20px] border-b">
+          <h3 className="block text-center pb-[15px] text-[18px] text-[#333] font-sans font-bold capitalize">
+            Different Views
+          </h3>
+          <ul className="flex justify-center gap-x-[10px]">
+            {imageSrcs.map((src) => (
+              <li
+                className="my-[30px] mx-auto w-[70px] h-[70px] p-[10px]
+                        bg-white shadow-xl flex justify-center items-center
+                        rounded-full"
+              >
+                <Image image={src} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="py-[20px] border-b flex justify-between">
+          <h3 className="block text-center text-[18px] text-[#333] font-sans font-bold capitalize">
+            Base Experience
+          </h3>
+          <span className="block text-center text-[18px] text-[#333] font-sans font-bold">
+            {PokemonData.base_experience}
+          </span>
+        </div>
+        <div className="py-[20px] border-b flex justify-between">
+          <h3 className="block text-center text-[18px] text-[#333] font-sans font-bold capitalize">
+            Order
+          </h3>
+          <span className="block text-center text-[18px] text-[#333] font-sans font-bold">
+            {PokemonData.order}
+          </span>
+        </div>
+        </>}
+      </div>
+      <Button value="View Details" onClick={()=>viewDetailHandler(PokemonData.name)}/>
+    </div>
+  );
+};
 export default PokemonCard;
+
+const toKilograms = (lbs) => {
+    return lbs / 10;
+  };
+  const toCentimeters = (val) => {
+    return val * 10;
+  };
--- src/Pages/PokemoneDetail/PokemoneDetail.js
+++ src/Pages/PokemoneDetail/PokemoneDetail.js
@@ -1,4 +1,14 @@
-const PokemoneDetail =()=> {
-    return <h1>Hello from PokemoneDetail</h1>
-}
+import { useLocation } from "react-router";
+import { PokemonCard } from "../../Components";
+
+const PokemoneDetail = () => {
+  const location = useLocation();
+  const name = location.state.name;
+
+  return (
+    <div className="flex justify-center">
+      <PokemonCard pokemone={name} page="detail" />
+    </div>
+  );
+};
 export default PokemoneDetail;
