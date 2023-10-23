ee3806bf77a336861b7d03a107298e5c48b9d1aa
mehranali717 <Mehranalee110@gmail.com>
2023-10-23 13:47:47 +0500

    generation upgraded to pagination

--- src/Pages/Generation/Generation.js
+++ src/Pages/Generation/Generation.js
@@ -1,31 +1,43 @@
 import { useLocation } from "react-router";
 import { useGetPokemonByGenerationQuery } from "../../Redux/Apis/ApiSlice";
-import { PageTitle, PokemonCard } from "../../Components";
+import { Button, PageTitle, PokemonCard } from "../../Components";
 import Loader from "../../Components/Loader/Loader";
+import { useState } from "react";
 
 const Generation = () => {
   const location = useLocation();
+  const [page, setPage] = useState(1);
+  const perPage = 6;
   const name = location.state.name;
   const {
     data: pokemonByGeneration,
     isLoading,
     isSuccess,
     error:loadingError
-  } = useGetPokemonByGenerationQuery(name);
-
+  } = useGetPokemonByGenerationQuery(name, {page, perPage});
+  const startIndex = (page - 1) * perPage;
+  const lastIndex = startIndex + perPage;
   return (
     <div className="flex flex-col gap-y-5">
       <PageTitle title={name} />
       {isLoading && <Loader />}
       {loadingError && <h1 className="text-[red] font-bold text-[20px]">Error While Loading Data</h1>}
       {isSuccess && (
-        <div className="scrollable flex flex-wrap justify-between gap-y-10 scrollablePokemoneOuter">
-          {pokemonByGeneration.pokemon_species.map((pokemone, index) => (
+       <>
+         <div className="scrollable flex flex-wrap justify-between gap-y-10 scrollablePokemoneOuter">
+          {pokemonByGeneration.pokemon_species.length > 0 && pokemonByGeneration.pokemon_species.slice(startIndex, lastIndex).map((pokemone, index) => (
             <PokemonCard pokemone={pokemone.name} key={index} />
           ))}
         </div>
+        <div className="flex gap-x-5 justify-between self-end">
+            {page > 1 && (
+              <Button value="Prev" onClick={() => setPage(page - 1)} />
+            )}
+            <Button value="Next" onClick={() => setPage(page + 1)} />
+        </div>
+       </>
       )}
     </div>
   );
-};
+  };
 export default Generation;
